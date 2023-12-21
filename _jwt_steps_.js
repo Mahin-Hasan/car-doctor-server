d53fb218773b40f52cd31df6ac116e7d9787c923
Mahin-Hasan <mahinmtrs@gmail.com>
2023-12-22 04:25:42 +0600

    implemented JWT secret token

---
+++ _jwt_steps_.js
@@ -0,0 +1,13 @@
+/*
+1. install jsonwebtoken
+2. jwt.sign (payload, secret, {expireIn:})
+3. token client
+
+
+===========
+## How to store token in the client side
+1. memory - ok
+2. local storage - ok 
+3. cookies: http only - good
+
+*/
--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const express = require('express');
 const cors = require('cors');
+const jwt = require('jsonwebtoken');
 require('dotenv').config()
 const app = express();
 const port = process.env.PORT || 5000;
@@ -33,6 +34,15 @@ async function run() {
         const serviceCollection = client.db('carDoctor').collection('services');
         const bookingCollection = client.db('carDoctor').collection('bookings');
 
+        //app related api
+        app.post('/jwt', async (req, res) => {
+            const user = req.body;
+            console.log(user);
+            const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1h' })
+            res.send(token);
+        })
+
+        //service related api
         app.get('/services', async (req, res) => {
             const cursor = serviceCollection.find();
             const result = await cursor.toArray();
@@ -76,7 +86,7 @@ async function run() {
             const updatedBooking = req.body;
             console.log(updatedBooking);
             const updateDoc = {
-                $set:{
+                $set: {
                     status: updatedBooking.status
                 },
             };
